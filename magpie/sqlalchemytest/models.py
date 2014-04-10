d9b00c7e95dac42b066206646315be484b27f157
nimiq <puntonim@gmail.com>
2014-04-11 01:11:54 +0200

    Code snippets to work w/ SQLAlchemy

--- magpie/sqlalchemytest/models.py
+++ magpie/sqlalchemytest/models.py
@@ -1,37 +1,35 @@
-from sqlalchemy import create_engine, Column, Integer, String, Enum
-from magpie.settings import settings
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
 
 Base = declarative_base()
-
-
-_engine = None
-def start_engine():
-    global _engine
-    if not _engine:
-        _engine = create_engine('{}{}'.format(settings.DATABASE['ENGINE'],
-                                              settings.DATABASE['NAME']),)
-    return _engine
-engine = start_engine()
-
-
-def setup_db():
-    Base.metadata.create_all(engine)
 
 
 class Pet(Base):
     __tablename__ = "pet"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)  # Automatically generated if not provided
     type = Column(String(16))
-    breed = Column(String(32))
-    gender = Column(Enum('male', 'female'))
-    name = Column(String(64))
+    breed = Column(String(32))  # default: nullable=True
+    gender = Column(Enum('male', 'female'))  # Accepts only 'male' or 'female' (not nullable)
+    name = Column(String(64), nullable=False)  # This is not nullable
+
+    # Set the related field on the other table (pethouse.id)
+    pethouse_id = Column(Integer, ForeignKey('pethouse.id'))
+    # Set the other table (PetHouse) and the name of the reverse relationship (pets)
+    pethouse = relationship("PetHouse", backref=backref('pets', order_by=id))
 
     def __repr__(self):
         return "<Pet(id={}, name={}>".format(self.id, self.name)
 
 
+class PetHouse(Base):
+    __tablename__ = "pethouse"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(16))
+    address = Column(String(16))
 
 
 
