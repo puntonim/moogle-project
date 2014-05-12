479d53501377918145a2a983270f0178b7b33d07
nimiq <puntonim@gmail.com>
2014-05-12 23:56:57 +0200

    Fix Dropbox Solr delete lowercase issue

--- magpie/dropboxlib/indexer/solrupdater.py
+++ magpie/dropboxlib/indexer/solrupdater.py
@@ -7,6 +7,7 @@ from utils.exceptions import SolrResponseError
 from utils.dates import dropbox_date_to_solr_date
 from magpie.settings import settings
 from models import Provider
+from utils.solr import escape_solr_query
 
 
 log = logging.getLogger('dropbox')
@@ -20,14 +21,19 @@ class DropboxSolrUpdater:
         self.url = '{}/{}/update'.format(settings.SOLR_URL, self.CORE_NAME)
 
     def add(self, redis_entry, commit=False):
+        """
+        Add a new file to the Solr index for the current `bearertoken_id`.
+
+        Parameters:
+        redis_entry -- a `RedisDropboxEntry` instance.
+        """
         local_file_path = normpath(join(settings.DROPBOX_TEMP_STORAGE_PATH,
                                         str(self.bearertoken_id),
                                         redis_entry.local_name))
 
         # Build Solr doc.
         doc = self._convert_redis_entry_to_solr_doc(redis_entry, local_file_path)
-        r = self._post_file(doc, local_file_path)
-        self._sanity_check(r, True)
+        self._post_file(doc, local_file_path)
 
         if commit:
             self.commit()
@@ -43,7 +49,7 @@ class DropboxSolrUpdater:
 
         doc = dict()
         doc['literal.bearertoken_id'] = self.bearertoken_id
-        doc['literal.id'] = redis_entry.id
+        doc['literal.id'] = '{}:{}'.format(self.bearertoken_id, redis_entry.id)
         doc['literal.remote_path'] = metadata['path']
         doc['literal.modified_at'] = dropbox_date_to_solr_date(metadata['modified'])
         doc['literal.mime_type'] = metadata['mime_type']
@@ -51,47 +57,55 @@ class DropboxSolrUpdater:
 
         return doc
 
-    def delete(self, redis_entry):
+    def delete(self, redis_entry, commit=False):
         """
-        Delete `redis_entry`.
-        """
-        pass
+        Delete a Dropbox file from the Solr index for the current `bearertoken_id`.
 
-    def reset(self, commit=False):
+        Parameters:
+        redis_entry -- a `RedisDropboxEntry` instance.
         """
-        Delete all docs for `bearertoken_id`.
-        """
-        q = 'bearertoken_id:{}'.format(self.bearertoken_id)
-        xml_data = '<delete><query>{}</query></delete>'.format(q)
-        r = self._post_xml(xml_data)
-        self._sanity_check(r)
+
+        # We want to delete the specific entry and all its children, in case of any.
+        # Note: '/' is a keyword in Solr, so we need to escape it this way: '\/'.
+        # Note: keep in mind that there is no way to know if the entry is a folder or a file (cause
+        # as this is a entry to delete, Dropbox doesn't send us metadata, and we cannot check
+        # what is already in our index, because we don't index folders). But this is not a problem.
+        #
+        # First we delete the item: remote_path:\/folder1\/folder2\/folder\ 3
+        # Then all children, if any: remote_path:\/folder1\/folder2\/folder\ 3\/*
+        # Or all together:
+        #   remote_path:(\/folder1\/folder2\/folder\ 3 OR \/folder1\/folder2\/folder\ 3\/*)
+        #
+        # Note: this is smart because we don't delete: /folder1/folder2/folder 30
+        root = escape_solr_query(redis_entry.remote_path)
+        children = '{}\/*'.format(root)
+        q = 'remote_path_ci:({} OR {}) '.format(root.lower(), children.lower()) + \
+            'AND bearertoken_id:{}'.format(self.bearertoken_id)
+        self._delete_by_query(q)
 
         if commit:
             self.commit()
 
-        return r
+    def reset(self, commit=False):
+        """
+        Delete all files from the Solr index for the current `bearertoken_id`.
+        """
+        q = 'bearertoken_id:{}'.format(self.bearertoken_id)
+        self._delete_by_query(q)
+
+        if commit:
+            self.commit()
 
     def commit(self):
         xml_data = '<commit waitSearcher="true" expungeDeletes="false"/>'
         r = self._post_xml(xml_data)
+        self._sanity_check(r, False)
+
+    def _delete_by_query(self, query):
+        xml_data = '<delete><query>{}</query></delete>'.format(query)
+        print(xml_data)
+        r = self._post_xml(xml_data)
         self._sanity_check(r)
-        return r
-
-    @staticmethod
-    def _sanity_check(r, is_check_solr_response=False):
-        if r.status_code != 200:
-            raise SolrResponseError('HTTP Status: {}\n{}'.format(
-                r.status_code, json.dumps(json.loads(r.text), indent=4))
-            )
-
-        if not is_check_solr_response:
-            return
-
-        solr_status = json.loads(r.text)['responseHeader']['status']
-        if solr_status != 0:
-            raise SolrResponseError('Solr Status: {}\n{}'.format(
-                solr_status, json.dumps(json.loads(r.text), indent=4))
-            )
 
     def _post_file(self, doc, local_file_path):
         """
@@ -108,7 +122,7 @@ class DropboxSolrUpdater:
         log.debug('Posting file to Solr: {}\nParams: {}'.format(self.url, params))
         # Send request.
         r = requests.post('{}/extract'.format(self.url), params=params, files=files)
-        return r
+        self._sanity_check(r)
 
     def _post_xml(self, xml):
         """
@@ -122,5 +136,23 @@ class DropboxSolrUpdater:
             'Content-type': 'text/xml; charset=utf-8',
             'Content-Length': "%s" % len(xml_data)
         }
-        r = requests.post(self.url, data=xml_data, headers=headers)
+        params = dict()
+        params['wt'] = 'json'
+        r = requests.post(self.url, params=params, data=xml_data, headers=headers)
         return r
+
+    @staticmethod
+    def _sanity_check(r, is_check_solr_response=True):
+        if r.status_code != 200:
+            raise SolrResponseError('HTTP Status: {}\n{}'.format(
+                r.status_code, json.dumps(json.loads(r.text), indent=4))
+            )
+
+        if not is_check_solr_response:
+            return
+
+        solr_status = json.loads(r.text)['responseHeader']['status']
+        if solr_status != 0:
+            raise SolrResponseError('Solr Status: {}\n{}'.format(
+                solr_status, json.dumps(json.loads(r.text), indent=4))
+            )
