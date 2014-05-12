eff924e07999bd0aaaa36373c658efb1ffefe5c7
nimiq <puntonim@gmail.com>
2014-05-12 23:56:03 +0200

    Add method to escape special chars in Solr queries

--- magpie/utils/solr.py
+++ magpie/utils/solr.py
@@ -12,3 +12,15 @@ def open_solr_connection(core_name):
         url = '{}/{}'.format(settings.SOLR_URL, core_name)
         _solr = Solr(url)
     return _solr
+
+
+def escape_solr_query(query):
+    """
+    Escape special chars for Solr queries.
+    """
+    chars = ['+', '-', '&&', '||', '!', '(', ')', '{', '}', '[', ']', '^', '"', '~', '*', '?',
+             ':', '/', ' ']
+    for char in chars:
+        query = query.replace(char, '\{}'.format(char))
+
+    return query
