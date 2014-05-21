f60ae010c61d1fdfd97914da6d81382a14d09913
nimiq <puntonim@gmail.com>
2014-05-21 10:53:43 +0200

    Update TEST_BEARERTOKEN_IDS

--- magpie/magpie/settings/base.py
+++ magpie/magpie/settings/base.py
@@ -19,7 +19,7 @@ DATABASE = {
 }
 
 # Test bearertoken_ids
-TEST_BEARERTOKEN_IDS = []
+TEST_BEARERTOKEN_IDS = range(0, 51)  # From 0 to 50 included.
 
 # Dropbox settings.
 DROPBOX_MAX_FILE_SIZE = 10*1024*1024  # 10 MB in bytes
