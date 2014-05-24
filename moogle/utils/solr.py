3ef8753803cd4e47b3c7897b35341d0c09b2fe60
nimiq <puntonim@gmail.com>
2014-05-24 19:04:43 +0200

    Fix a typo

--- moogle/utils/solr.py
+++ moogle/utils/solr.py
@@ -35,7 +35,7 @@ class Solr:
     @property
     def _mysolr(self):
         # TODO: This thing of caching mysolr will be useless with mysolr 0.9, because it has the
-        # TODO: ability to resuse the same requests.session, like:
+        # TODO: ability to re-use the same requests.session, like:
         # TODO: session = requests.Session()
         # TODO: solr = Solr('http://localhost:8983/solr/collection1', make_request=session)
         # TODO: see http://mysolr.redtuna.org/en/latest/user/userguide.html
@@ -53,17 +53,6 @@ class Solr:
         r = self._mysolr.search(*args, **kwargs)
         self._sanity_check(r, True)
         return r
-
-    def search_cursor(self, *args, **kwargs):
-        """
-        Search in Solr using a cursor to navigate next results.
-        It uses mysolr library.
-        """
-        # TODO: change this such as there is an automatic sanity_check
-        # TODO: then update also change_bearertoken_by_query() in solr_extra.py
-        cur = self._mysolr.search_cursor(*args, **kwargs)
-        ####self._sanity_check(r, True)
-        return cur
 
     def commit(self):
         """
