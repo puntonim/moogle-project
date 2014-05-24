7b47424f5a5e5c462e421133241981e983d81d54
nimiq <puntonim@gmail.com>
2014-05-24 23:24:50 +0200

    Move results templates to separate files

--- moogle/search/snooper/dropbox.py
+++ moogle/search/snooper/dropbox.py
@@ -6,4 +6,5 @@ class DropboxSnooper(BaseSolrSnooper):
     def __init__(self, user):
         self.user = user
         self.provider_name = Provider.NAME_DROPBOX
-        self.extra_query_args = {'fl': 'id'}
+        self.extra_query_args = {
+            'fl': 'id,title,bytes,mime_type,content_type,remote_path,modified_at,'}
